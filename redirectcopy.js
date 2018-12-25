ea0bd8e6ab8721f866090b260c01601cd1431182
bichnga <44836923+bichnga@users.noreply.github.com>
2018-12-25 21:38:03 +0700

    Update redirectcopy.js

--- redirectcopy.js
+++ redirectcopy.js
@@ -2,7 +2,7 @@ function FSAddOriginalLink(){
     var body_element = document.getElementsByTagName('body')[0];
     var selection;
     selection = window.getSelection();
-    var pagelink = "<br/><br/>MUON COPY THI VAO DAY : <a href='https://datnenthanhphobienhoa.blogspot.com/'>https://datnenthanhphobienhoa.blogspot.com/"+document.location.pathname+"</a>";
+    var pagelink = "<br/><br/>MUON COPY THI VAO DAY : <a href='https://www.facebook.com/MatOngNguyenChat.DamBaoChatLuong.GiaRe/?modal=admin_todo_tour/'>https://www.facebook.com/MatOngNguyenChat.DamBaoChatLuong.GiaRe/?modal=admin_todo_tour/"+document.location.pathname+"</a>";
     var copytext = pagelink;
     var newdiv = document.createElement('div');
     newdiv.style.position='absolute';
